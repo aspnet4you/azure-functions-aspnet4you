24a0b2be358c868b1cc383f9dea4085c0418bd68
Prodip K Saha <prodip.kumar@outlook.com>
2018-02-21 23:03:32 -0600

    HttpTrigger with POCO IN parameter How to insert a message in Azure Storage Queue QueueTrigger to read messages from Azure Storage Queue How to use OUT parameter with IAsyncCollector<T> SendGrid to deliver emails

---
+++ AzureFunctionApps/QueueOrder.cs
@@ -0,0 +1,78 @@
+using System.Linq;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage; 
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace AzureFunctionApps
+{
+    /// <summary>
+    /// QueueOrder: Insert a POCO order object to Azure Queue Storage. Parameter order object must be defined as HttpTrigger.
+    /// This is needed for the object to be deserialized automatically by Azure Function. Req object should not be defined at HttpTrigger.
+    /// https://docs.microsoft.com/en-us/azure/storage/queues/storage-dotnet-how-to-use-queues
+    /// </summary>
+    public static class QueueOrder
+    {
+        [FunctionName("QueueOrder")]
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, 
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] POCOOrder order, TraceWriter log)
+        {
+            string msg = $"Hello " + order.CustomerName + "! Your order for " + order.ProductName + " with quantity of " + order.OrderCount + " has been received. You will receive email at " + order.CustomerEmail + " as soon as order is shipped.";
+            log.Info(msg);
+
+            var connectionString = GetEnvironmentVariable("AzureWebJobsStorage");
+
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a container.
+            CloudQueue queue = queueClient.GetQueueReference("orderqueue");
+
+            // Create the queue if it doesn't already exist
+            queue.CreateIfNotExists();
+
+            // Serialize POCO Order
+            var orderData = JsonConvert.SerializeObject(order, Formatting.Indented);
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage(orderData);
+            queue.AddMessage(message);
+
+            // Send an acknowledgement to client app
+            return await Task.Run(() =>
+            {
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(msg, Formatting.Indented), Encoding.UTF8, "application/json")
+                };
+            });
+        }
+
+        private static string GetEnvironmentVariable(string name)
+        {
+            return System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+    }
+
+    /// <summary>
+    /// This is just a simple/sample object class to demonstrate intake of parameter as POCO object.
+    /// </summary>
+    public class POCOOrder
+    {
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int OrderCount { get; set; }
+    }
+}
---
+++ AzureFunctionApps/SendEmail.cs
@@ -0,0 +1,47 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+
+namespace AzureFunctionApps
+{
+    /// <summary>
+    /// SendEmail: This function is triggered when an order is queued into Azure Queue Storage. Additionally, this function sends Email
+    /// using SendGrid and IAsyncCollector<T> as output parameter. All you have to do is- add a message to output parameter!
+    /// </summary>
+    public static class SendEmail
+    {
+        [FunctionName("SendEmail")]
+        public static async void Run([QueueTrigger("orderqueue", Connection = "AzureWebJobsStorage")]POCOOrder order, 
+            TraceWriter log, [SendGrid( ApiKey = "AzureWebJobsSendGridApiKey")] IAsyncCollector<Mail>  outputMessage)
+        {
+            string msg = $"Hello " + order.CustomerName + "! Your order for " + order.ProductName + " with quantity of " + order.OrderCount + " has been shipped.";
+            log.Info(msg);
+
+            Mail message = new Mail
+            {
+                Subject = msg,
+                From = new Email("Prodip.Kumar@outlook.com")
+            };
+
+            
+            var personalization = new Personalization();
+            // Change To email of recipient
+            personalization.AddTo(new Email(order.CustomerEmail)); 
+
+            Content content = new Content
+            {
+                Type = "text/plain",
+                Value = msg + "Thank you for being such a nice customer!"
+            };
+            message.AddContent(content);
+            message.AddPersonalization(personalization);
+            
+
+            await outputMessage.AddAsync(message);
+        }
+    }
+}
